9ecbf7bac58d191c9eedcece8ccf78affa24cee2
cdieringer3 <cdieringer3@gatech.edu>
2016-02-24 21:42:25 -0700

    stage eslint with precommit validation

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "extends": "eslint:recommended",
+    "env": {
+        "node": true,
+        "commonjs": true,
+        "mocha": true,
+        "es6": true
+    },
+    "rules": {
+        "linebreak-style": [
+            2,
+            "unix"
+        ]
+
+    }
+};
